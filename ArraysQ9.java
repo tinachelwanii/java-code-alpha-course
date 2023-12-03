625467ab1e8ef4d47434c6269fc8821d9545b71b
tina <102860264+tinachelwanii@users.noreply.github.com>
2023-12-04 01:58:19 +0530

    Add files via upload

---
+++ ArraysQ9.java
@@ -0,0 +1,24 @@
+
+//pairs in an array
+import java.util.*;
+
+public class ArraysQ9 {
+    public static void printPairs(int numbers[]) {
+        int tp = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.println("(" + curr + "," + numbers[j] + ") ");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("total pairs =" + tp);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        printPairs(numbers);
+    }
+}
