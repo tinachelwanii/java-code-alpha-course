4e01959d07c4fd50604b127552f9ac6ae1b4da1a
tina <102860264+tinachelwanii@users.noreply.github.com>
2023-12-06 15:39:43 +0530

    Add files via upload

---
+++ Operators15.java
@@ -0,0 +1,16 @@
+
+//assignment operators
+import java.util.*;
+
+public class Operators15 {
+    public static void main(String[] args) {
+        int A = 10;
+        // A = A+ 10;
+        A += 10;
+        int B = 5;
+        // B=B-5;
+        B -= 5;
+        // System.out.println(A);
+        System.out.println(B);
+    }
+}
