ccbe1252ca3eda44c97837e0728fd6ad32ed9a34
tina <102860264+tinachelwanii@users.noreply.github.com>
2023-12-07 19:26:22 +0530

    Add files via upload

---
+++ loops10.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class loops10 {
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.println("hello world");
+            counter++;
+        } while (counter <= 10);
+    }
+
+}
